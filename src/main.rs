d186031f6274e974026e3509de76336c7d5c41fa
ilarionkuleshov <ilarion.kuleshov@gmail.com>
2025-06-08 00:16:53 +0300

    feat: update main

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+mod keyboard;
+mod playground;
+mod screen;
+mod snake;
+
+use crossterm::terminal;
+
 fn main() {
-    println!("Hello from Rust Snake Game!");
+    terminal::enable_raw_mode().unwrap();
+
+    let playground = playground::Playground::new();
+    let mut snake = snake::Snake::new(&playground);
+
+    loop {
+        let keyboard_event = keyboard::get_keyboard_event(100);
+
+        snake.update(&playground, keyboard_event);
+        screen::clear();
+        screen::draw(&playground, &snake);
+
+        if !snake.is_alive {
+            print!("Game Over!");
+            break;
+        }
+    }
+    terminal::disable_raw_mode().unwrap();
 }
